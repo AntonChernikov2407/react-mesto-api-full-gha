2e4a8491547f417edc0b880aa7eae00959ff0219
Anton Chernikov <000tony00053@gmail.com>
2023-07-10 17:50:57 +0300

    fix: исправить ошибки

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -99,6 +99,7 @@ class Api {
 
 }
 const jwt = localStorage.getItem('jwt');
+console.log(jwt);
 const api = new Api({
   baseUrl: 'https://api.mesto.anton-chernikov.nomoredomains.work',
   headers: {
