ef99b060ec3892d49e658ff9a4ba513efe719bdd
Anton Chernikov <000tony00053@gmail.com>
2023-07-10 16:25:17 +0300

    fix: исправить ошибки

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -10,14 +10,14 @@ const cors = (req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', '*');
-    const { method } = req;
-    const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-    const requestHeaders = req.headers['access-control-request-headers'];
-    if (method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-      res.header('Access-Control-Allow-Headers', requestHeaders);
-      res.end();
-    }
+    // const { method } = req;
+    // const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+    // const requestHeaders = req.headers['access-control-request-headers'];
+    // if (method === 'OPTIONS') {
+    //   res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    //   res.header('Access-Control-Allow-Headers', requestHeaders);
+    //   res.end();
+    // }
   }
 
   next();
