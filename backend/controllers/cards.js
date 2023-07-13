bf4db83f279d78f8a5f7ca08db59130a31c78437
Anton Chernikov <000tony00053@gmail.com>
2023-07-13 17:48:36 +0300

    fix: исправить ошибки

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -28,7 +28,7 @@ const deleteCardById = (req, res, next) => {
 const createCard = (req, res, next) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.status(201).send({ data: card }))
+    .then((data) => res.status(201).send(data))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         next(new ValidationError(err.message));
--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -9,7 +9,7 @@ const allowedCors = [
 const cors = (req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
     const { method } = req;
     const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
     const requestHeaders = req.headers['access-control-request-headers'];
